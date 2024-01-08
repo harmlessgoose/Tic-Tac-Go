2838d5b430865bfed344499f8e1759ea0c5a7f26
Peter O <65027464+harmlessgoose@users.noreply.github.com>
2024-01-08 17:22:57 +0000

    Get input

--- main.go
+++ main.go
@@ -19,17 +19,43 @@ func main() {
 		{"-", "-", "-"},
 	}
 
+	playerturn := 1
+
+	fmt.Println("Welcome to Tic Tac Go!\n")
 
 	for !gameOver {
 		// print board
 		printBoard(board)
 
 		// get input
+		var row, col int
+
+		fmt.Print("Enter two numbers for the row and column (e.g., 1 3): ")
+        _, err := fmt.Scan(&row, &col)
+
+		if row < 1 || row > 3 || col < 1 || col > 3 {
+			fmt.Println("Invalid input, please enter two integers between 1 and 3.")
+			continue
+		}
+
+        if err != nil {
+            fmt.Println("Invalid input, please enter two integers between 1 and 3.")
+            continue
+        }
+
+		if board[row-1][col-1] != "-" {
+			fmt.Println("That spot is already taken, please choose another.")
+			continue
+		}
 
 		// update board
+		if playerturn == 1 {
+			board[row-1][col-1] = "X"
+		} else {
+			board[row-1][col-1] = "O"
+		}
 
 		// check for win
-		gameOver = checkWin()
 
 		// check for draw
 
@@ -48,4 +74,5 @@ func printBoard(board [][]string) {
 	for _, row := range board {
 		fmt.Println(row)
 	}
+	fmt.Println()
 }
